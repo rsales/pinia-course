98b6baf5444992ef7c0365045f5c3060bf784abe
rsales <rafael.sales93@hotmail.com>
2022-08-06 12:12:35 -0300

    01x06 Synchronous and Asynchronous Actions

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
-const productStore = useProductStore();
 
-// import { useProductStore } from "@/stores/ProductStore";
-// import { storeToRefs } from "pinia";
-// const { products } = storeToRefs(useProductStore());
+const productStore = useProductStore();
+productStore.fill();
 </script>
 
 <template>
--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,15 +1,19 @@
 import { defineStore } from "pinia";
-import products from "@/data/products.json"
 
 export const useProductStore = defineStore("ProductStore", {
   // state
   state: () => {
     return {
-      products,
-    }
-  }
+      products:[],
+    };
+  },
 
   // actions
+  actions: {
+    async fill() {
+      this.products = (await import("@/data/products.json")).default;
+    },
+  }
 
   // getters
 })
