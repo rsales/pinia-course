eb7fab8a2955ef61e6d2f001adb251bbd81f7bbb
rsales <rafael.sales93@hotmail.com>
2022-08-06 12:01:38 -0300

    01x05 Access State from a Pinia Store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
 import { useProductStore } from "@/stores/ProductStore";
+const productStore = useProductStore();
 
-useProductStore();
+// import { useProductStore } from "@/stores/ProductStore";
+// import { storeToRefs } from "pinia";
+// const { products } = storeToRefs(useProductStore());
 </script>
 
 <template>
@@ -12,7 +14,7 @@ useProductStore();
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
       />
